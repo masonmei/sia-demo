a3b99408e1103e72defa0889ec6385ee8137bf34
Mason <mason@igitras.com>
2015-12-10 23:40:52 +0800

    fix bugs

--- src/main/java/com/baidu/oped/sia/business/configuration/ApplicationProperties.java
+++ src/main/java/com/baidu/oped/sia/business/configuration/ApplicationProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * Application Propertity Configuration
+ * Application Propertity Configuration.
  *
  * @author mason
  */
