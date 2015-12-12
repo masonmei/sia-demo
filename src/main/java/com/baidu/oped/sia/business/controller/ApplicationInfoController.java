a067e2094596c96e4104924f7f684d8c9fb18590
Mason <mason@igitras.com>
2015-12-12 23:27:04 +0800

    change the check style

--- src/main/java/com/baidu/oped/sia/business/controller/ApplicationInfoController.java
+++ src/main/java/com/baidu/oped/sia/business/controller/ApplicationInfoController.java
@@ -1,6 +1,7 @@
 package com.baidu.oped.sia.business.controller;
 
 import com.baidu.oped.sia.business.configuration.ApplicationProperties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/baidu/oped/sia/business/controller/ApplicationInfoControllerTest.java
+++ src/test/java/com/baidu/oped/sia/business/controller/ApplicationInfoControllerTest.java
@@ -1,6 +1,15 @@
 package com.baidu.oped.sia.business.controller;
 
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 import com.baidu.oped.sia.business.Application;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,13 +20,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by mason on 12/8/15.
