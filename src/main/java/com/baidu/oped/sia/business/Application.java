74021d5432ce4276cf504b86f4c7de8531079d44
Mason <mason@igitras.com>
2015-12-21 22:21:52 +0800

    fix bugs for exception handler

--- src/main/java/com/baidu/oped/sia/business/Application.java
+++ src/main/java/com/baidu/oped/sia/business/Application.java
@@ -1,15 +1,24 @@
 package com.baidu.oped.sia.business;
 
+import com.baidu.oped.sia.boot.exception.SystemExceptionHandler;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 /**
  * demo application entrance.
  *
  * @author mason
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
+
+    @Bean
+    public SystemExceptionHandler exceptionHandler() {
+        return new SystemExceptionHandler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
